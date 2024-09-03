3300fe5ce0ea82846215c4ce9349867fc6dac475
Adik Soleh <adiksoleh4@gmail.com>
2024-09-03 15:38:31 +0700

    commit

--- days7/js/humber.js
+++ days7/js/humber.js
@@ -3,6 +3,10 @@
 const menuTogggle = document.querySelector('.menu-toggle input');
 const nav = document.querySelector('.nav ul');
 
+
+
+
+
 menuTogggle.addEventListener('click',function (){
     nav.classList.toggle('box-sld');
 }); 
