9b7933ad79cd1217f8b3365df6d5cea85dfb1b40
Adik Soleh <adiksoleh4@gmail.com>
2024-09-07 17:51:53 +0700

    commi

--- days8/js/HOF.js
+++ days8/js/HOF.js
@@ -2,49 +2,47 @@
 
 const CheckRating = [
   {
-    autor : "- Naruto",
-    quote : "Prinsip keadilanku adalah memberi makanan pada yang kelaparan.",
-    image : "https://www.pngitem.com/pimgs/m/418-4181169_thumb-image-naruto-jpg-hd-png-download.png",
-    rating : 5
+    autor: "- Naruto",
+    quote: "Takdir setiap manusia memang telah ditentukan sejak mereka lahir, tetapi dengan kerja keras kita dapat mengalahkan takdir",
+    image: "https://www.pngitem.com/pimgs/m/418-4181169_thumb-image-naruto-jpg-hd-png-download.png",
+    rating: 5
   },
   {
-    autor : "- Sasuke",
-    quote : "Aku tidak peduli, walaupun aku harus mati untuk mengejar impianku(ngoding)",
-    image : "https://i.pinimg.com/736x/35/e6/be/35e6be85791848115519fc6566924c1a.jpg",
-    rating : 4
+    autor: "- Sasuke",
+    quote: "Rasa sakit nyatanya bisa membuat orang lebih kuat.",
+    image: "https://i.pinimg.com/736x/35/e6/be/35e6be85791848115519fc6566924c1a.jpg",
+    rating: 4
   },
   {
-    autor : "- Sakura",
-    quote : "Jangan pernah meremahkan diri sendiri, karena diri sendiri ada kelebihan tersendiri.",
-    image : "https://i.pinimg.com/736x/73/83/41/738341e4fe7280f80a6cca75e626797b.jpg",
-    rating : 1
+    autor: "- Sakura",
+    quote: "Senyum adalah cara termudah untuk keluar dari situasi yg sulit",
+    image: "https://i.pinimg.com/736x/73/83/41/738341e4fe7280f80a6cca75e626797b.jpg",
+    rating: 1
   },
   {
-    autor : "- Kakashi",
-    quote : "Ketika dunia jahat kepadamu, maka berusahalah untuk menghadapinya, karena tidak ada orang yang membantumu jika kau tidak berusaha.",
-    image : "https://i.pinimg.com/474x/8b/bf/c3/8bbfc35c176cf377d2f350db6a9af56d.jpg",
-    rating : 2
+    autor: "- Kakashi",
+    quote: "Dalam hidup bermasyarakat, mereka yang tidak memiliki banyak kemampuan cenderung lebih banyak mengeluh.",
+    image: "https://i.pinimg.com/474x/8b/bf/c3/8bbfc35c176cf377d2f350db6a9af56d.jpg",
+    rating: 2
   },
   {
-    autor : "- Madara Uchiha",
-    quote : "Hidup ini seperti pensil yang lama lama akan habis, tetapi akan meninggalkan tulisan yang indah dalam kehidupan",
-    image : "https://i.pinimg.com/736x/09/2b/63/092b63e2cfe5db87a5249aa7cff0d769.jpg",
-    rating : 4
+    autor: "- Madara Uchiha",
+    quote: "Hidup adalah teka-teki yang terjadi di masa-masa awal kita, tetapi seiring bertambahnya usia, kita mulai menyadari makna hidup dan pola yang terjadi.",
+    image: "https://i.pinimg.com/736x/09/2b/63/092b63e2cfe5db87a5249aa7cff0d769.jpg",
+    rating: 4
   },
   {
-    autor : "- Obito",
-    quote : "Hidup ini seperti pensil yang lama lama akan habis, tetapi akan meninggalkan tulisan yang indah dalam kehidupan",
-    image : "https://i.pinimg.com/564x/c1/2e/b1/c12eb1d17a49ff423e92dd6059a18972.jpg",
-    rating : 3
+    autor: "- Obito",
+    quote: "Mereka yang tidak mengikuti aturan adalah sampah. Tapi, mereka yang meninggalkan temannya bahkan lebih buruk dari sampah",
+    image: "https://i.pinimg.com/564x/c1/2e/b1/c12eb1d17a49ff423e92dd6059a18972.jpg",
+    rating: 3
   },
   {
-    autor : "- Boruto",
-    quote : "Hidup ini seperti pensil yang lama lama akan habis, tetapi akan meninggalkan tulisan yang indah dalam kehidupan",
-    image : "https://i.pinimg.com/736x/9e/bf/f4/9ebff459d6b33ceffef49bddd97c4fcc.jpg",
-    rating : 5
+    autor: "- Boruto",
+    quote: "Ketika ada sesuatu yang sangat kita inginkan betapapun besar keinginan kita itu, mendapatkannya dengan kekuatanmu sendiri jauh lebih bagus.",
+    image: "https://i.pinimg.com/736x/9e/bf/f4/9ebff459d6b33ceffef49bddd97c4fcc.jpg",
+    rating: 5
   }
-
-
 ]
 
 
